66a5b8075aa82c97eb230f433bc3e6cf0eb9d68f
Cristina <crissy6600@gmail.com>
2017-03-20 14:21:35 +0000

    rectangle function

--- script.js
+++ script.js
@@ -12,7 +12,34 @@ right(90);
 forward(50);
 }
 
+function triangle() {
+    forward(75);
+    right(120);
+    forward(75);
+    right(120);
+    forward(75);
+}
 // Type your function call below
-
-
+//triangle();
 stamp();
+
+function rectangle() {
+   
+    right(90);
+    forward(40);
+    right(90);
+    forward(80);
+    right(90);
+    forward(40);
+    right(90);
+    forward()
+}
+
+
+rectangle();
+stamp();
+
+
+
+
+
